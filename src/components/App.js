662ae17bbee316676f1beb23e3df55baed224d2e
Stephen <stephen.sant123@gmail.com>
2021-11-10 10:08:49 +1100

    Moved App into components

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>BURNING AIRLINES</h1>
+      <em>We put the AAAAAAAAAAAAAAAAAAAAAAHHHH in adventure.</em>
+    </div>
+  );
+}
+
+export default App;
