e7599d40c8204c5c39e887815a7fb5d833128536
Stephen <stephen.sant123@gmail.com>
2021-11-10 15:16:34 +1100

    Trying to make it work.

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,12 @@ function App() {
     <div>
       <h1>BURNING AIRLINES</h1>
       <em>We put the AAAAAAAAAAAAAAAAAAAAAAHHHH in adventure.</em>
-<<<<<<< HEAD
       <ol>
         <li>User will login.</li>
         <li>User will search for a flight.</li>
         <li>User will select a seat.</li>
       </ol>
-=======
       <Search />
->>>>>>> 06c62589df9ddc16fddd2ae21c7c390aab09b690
     </div>
   );
 }
