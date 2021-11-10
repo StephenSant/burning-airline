ccc275bcd76d8723d6ffd2d864a2646220366f93
Stephen <stephen.sant123@gmail.com>
2021-11-10 14:53:43 +1100

    aaaaaaaAAAAAAAAAAAAAAHHHHHHHHHHH

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,11 @@ function App() {
     <div>
       <h1>BURNING AIRLINES</h1>
       <em>We put the AAAAAAAAAAAAAAAAAAAAAAHHHH in adventure.</em>
+      <ol>
+        <li>User will login.</li>
+        <li>User will search for a flight.</li>
+        <li>User will select a seat.</li>
+      </ol>
     </div>
   );
 }
