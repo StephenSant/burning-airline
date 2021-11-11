1c4c1baa93f45fd8eb8b339c6ad5b8df7dd143e5
Matt <matthew.schultz1992@gmail.com>
2021-11-11 10:52:41 +1000

    Added Meow-Meow

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,23 +9,26 @@ class Search extends Component {
 
   constructor() {
     super();
-    this.state = {flights: []};
+    this.state = {flights: [], cat_gif: ''}
     this.fetchFlights = this.fetchFlights.bind(this);
   }
 
   fetchFlights(o, d) {
 
     let validFlights = [];
+    let cat_gif= '';
     axios(SERVER_URL).then((result) => {
     console.log(result.data)
+    if (o === 'Meow' && d === 'Meow-Meow') {
+      cat_gif = 'https://media3.giphy.com/media/lVBqPM2S0x3y0/giphy.gif?cid=ecf05e47zvz0js96fz81qz5b96gj5ld5jibnh77xfht7apbq&rid=giphy.gif&ct=g';
+    };
     for (let i = 0; i < result.data.length; i++) {
       if (result.data[i].origin === o && result.data[i].destination === d) {
         validFlights.push(result.data[i]);
       }
     }
-    this.setState({origin: o, destination: d, flights: validFlights});
+    this.setState({origin: o, destination: d, flights: validFlights, cat_gif: cat_gif});
     });
-
   }
 
   render() {
@@ -33,7 +36,7 @@ class Search extends Component {
       <div>
         <h1>Search Flights</h1>
         <SearchForm onSubmit={this.fetchFlights}/>
-        <ShowFlights flights={this.state.flights}/>
+        <ShowFlights flights={this.state.flights} cat_gif={this.state.cat_gif}/>
       </div>
     );
   }
@@ -44,7 +47,7 @@ const ShowFlights = (props) => {
     <div>
       {props.flights.map((f) =>
         <ul>
-          <li key={f.id}>Flight will leave from {f.origin} to {f.destination} on the {f.date}.</li>
+          <li key={f.id}>Flight will leave from {f.origin} to {f.destination} on the {f.date}.<img src={props.cat_gif} alt='Meow'></img></li>
         </ul>
       )}
     </div>
